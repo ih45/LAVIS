58691745a02f2a727107550ce613422b19139a54
ih45 <1759208001@qq.com>
2023-07-13 16:02:21 +0800

    add datasets for clotho caption

--- lavis/datasets/datasets/clotho_caption_datasets.py
+++ lavis/datasets/datasets/clotho_caption_datasets.py
@@ -8,7 +8,6 @@
 import os
 from collections import OrderedDict
 
-from torch.utils.data.dataloader import default_collate
 from lavis.datasets.datasets.base_dataset import BaseDataset
 # from PIL import Image
 import pandas as pd
@@ -31,7 +30,7 @@ class __DisplMixin:
         )
 
 
-class AudioTextPairDataset(BaseDataset, __DisplMixin):
+class ClothoCaptionDataset(BaseDataset, __DisplMixin):
     def __init__(self, audio_processor, text_processor, audio_root, caption_paths, which_caption):
         """
         audio_root (string): Root directory of audios (e.g. /mnt1/datasets/Clotho/clotho_audio_development/development/)
@@ -68,3 +67,42 @@ class AudioTextPairDataset(BaseDataset, __DisplMixin):
         self.audio_processor = audio_processor
         self.text_processor = text_processor
 
+class ClothoCapEvalDataset(BaseDataset, __DisplMixin):
+    def __init__(self, audio_processor, text_processor, audio_root, caption_paths, which_caption):
+        """
+        vis_root (string): Root directory of images (e.g. coco/images/)
+        ann_root (string): directory to store the annotation file
+        split (string): val or test
+        """
+        super().__init__(vis_processor=None, text_processor=text_processor, vis_root=None, ann_paths=[])
+
+        self.audio_processor = audio_processor
+        self.audio_root = audio_root
+        self.caption_paths = caption_paths
+        self.which_caption = which_caption # [caption_1|caption_2|caption_3|caption_4|caption_5]
+
+        for caption_path in self.caption_paths:
+            tmp_df = pd.read_csv(caption_path, encoding='utf-8')
+            self.annotation.extend(tmp_df.to_dict('records'))
+            # [{'file_name':, 'caption_1':, 'caption_2':, 'caption_3':, 'caption_4':, 'caption_5':}]
+
+        self._add_instance_ids()
+
+    def __getitem__(self, index):
+
+        # TODO: 不保证适用于其他数据集
+        ann = self.annotation[index]
+
+        audio_path = os.path.join(self.audio_root, ann["file_name"])
+
+        waveform = self.audio_processor(audio_path)
+
+        return {
+            "file_name": ann["file_name"],
+            "waveform": waveform,
+            "instance_id": ann["instance_id"]
+        }
+
+    def set_processors(self, audio_processor, text_processor):
+        self.audio_processor = audio_processor
+        self.text_processor = text_processor
